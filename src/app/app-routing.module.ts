8aeef042b0319b21b82b64677fc026ec25207c8e
ArthurMargarian <Artur.Margaryan@volo.global>
2021-03-14 21:05:17 +0400

    auth sturcture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './auth/auth.module#AuthModule'
+    // canActivate: [NotAuthGuard],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
